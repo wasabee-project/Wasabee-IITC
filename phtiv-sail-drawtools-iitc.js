08770107eb800b04b120bbd62b013103615d32c0
Steven Foskett <steven.foskett@gmail.com>
2018-11-28 17:26:55 -0600

    Added drawtools js file

---
+++ phtiv-sail-drawtools-iitc.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         PhtivSail Draw Tools
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Less terrible draw tools, hopefully.
+// @author       PhtivSail
+// @include      https://*.ingress.com/intel*
+// @include      http://*.ingress.com/intel*
+// @match        https://*.ingress.com/intel*
+// @match        http://*.ingress.com/intel*
+// @include      https://*.ingress.com/mission/*
+// @include      http://*.ingress.com/mission/*
+// @match        https://*.ingress.com/mission/*
+// @match        http://*.ingress.com/mission/*
+// @grant        none
+// ==/UserScript==
+
+// Wrapper function that will be stringified and injected
+// into the document. Because of this, normal closure rules
+// do not apply here.
+function wrapper(plugin_info) {
+  // Make sure that window.plugin exists. IITC defines it as a no-op function,
+  // and other plugins assume the same.
+  if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+  // Name of the IITC build for first-party plugins
+  plugin_info.buildName = 'hello';
+
+  // Datetime-derived version of the plugin
+  plugin_info.dateTimeVersion = '20150829103500';
+
+  // ID/name of the plugin
+  plugin_info.pluginId = 'hello';
+
+  // The entry point for this plugin.
+  function setup() {
+    alert('Hello, IITC!');
+  }
+
+  // Add an info property for IITC's plugin system
+  setup.info = plugin_info;
+
+  // Make sure window.bootPlugins exists and is an array
+  if (!window.bootPlugins) window.bootPlugins = [];
+  // Add our startup hook
+  window.bootPlugins.push(setup);
+  // If IITC has already booted, immediately run the 'setup' function
+  if (window.iitcLoaded && typeof setup === 'function') setup();
+}
+
+// Create a script element to hold our content script
+var script = document.createElement('script');
+var info = {};
+
+// GM_info is defined by the assorted monkey-themed browser extensions
+// and holds information parsed from the script header.
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
+  info.script = {
+    version: GM_info.script.version,
+    name: GM_info.script.name,
+    description: GM_info.script.description
+  };
+}
+
+// Create a text node and our IIFE inside of it
+var textContent = document.createTextNode('('+ wrapper +')('+ JSON.stringify(info) +')');
+// Add some content to the script element
+script.appendChild(textContent);
+// Finally, inject it... wherever.
+(document.body || document.head || document.documentElement).appendChild(script);
