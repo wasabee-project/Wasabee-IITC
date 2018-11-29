de6d91ac992ab2b7712636b8af3afb4429c683af
Steven Foskett <steven.foskett@gmail.com>
2018-11-28 18:33:45 -0600

    Made things actually exist

--- phtiv-sail-drawtools-iitc.js
+++ phtiv-sail-drawtools-iitc.js
@@ -19,52 +19,41 @@
 // into the document. Because of this, normal closure rules
 // do not apply here.
 function wrapper(plugin_info) {
-  // Make sure that window.plugin exists. IITC defines it as a no-op function,
-  // and other plugins assume the same.
-  if(typeof window.plugin !== 'function') window.plugin = function() {};
+    if (typeof window.plugin !== 'function')
+        window.plugin = function () {};
 
-  // Name of the IITC build for first-party plugins
-  plugin_info.buildName = 'hello';
+    //PLUGIN START
+    window.plugin.phtivsaildraw = function () {};
+    window.plugin.phtivsaildraw.loadExternals = function () {
+        try {
+            console.log('Loading arc.js now');
+        } catch (e) {
+        }
 
-  // Datetime-derived version of the plugin
-  plugin_info.dateTimeVersion = '20150829103500';
+        alert('Hello, IITC!');
 
-  // ID/name of the plugin
-  plugin_info.pluginId = 'hello';
+    };
 
-  // The entry point for this plugin.
-  function setup() {
-    alert('Hello, IITC!');
-  }
 
-  // Add an info property for IITC's plugin system
-  setup.info = plugin_info;
 
-  // Make sure window.bootPlugins exists and is an array
-  if (!window.bootPlugins) window.bootPlugins = [];
-  // Add our startup hook
-  window.bootPlugins.push(setup);
-  // If IITC has already booted, immediately run the 'setup' function
-  if (window.iitcLoaded && typeof setup === 'function') setup();
-}
 
-// Create a script element to hold our content script
+
+
+
+
+    //PLUGIN END
+    var setup = window.plugin.phtivsaildraw.loadExternals;
+
+    if (window.iitcLoaded && typeof setup === 'function') {
+        setup();
+    } else {
+        if (window.bootPlugins)
+            window.bootPlugins.push(setup);
+        else
+            window.bootPlugins = [setup];
+    }
+} // wrapper end
+// inject code into site context
 var script = document.createElement('script');
-var info = {};
-
-// GM_info is defined by the assorted monkey-themed browser extensions
-// and holds information parsed from the script header.
-if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
-  info.script = {
-    version: GM_info.script.version,
-    name: GM_info.script.name,
-    description: GM_info.script.description
-  };
-}
-
-// Create a text node and our IIFE inside of it
-var textContent = document.createTextNode('('+ wrapper +')('+ JSON.stringify(info) +')');
-// Add some content to the script element
-script.appendChild(textContent);
-// Finally, inject it... wherever.
+script.appendChild(document.createTextNode('(' + wrapper + ')();'));
 (document.body || document.head || document.documentElement).appendChild(script);
