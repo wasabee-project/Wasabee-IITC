2209bf09d531e7fe74e7444f6627e85aa13f8e27
QPotato <federicobadaloni@hotmail.com>
2019-06-23 01:58:53 -0300

    cleanup

--- src/code/auxiliar.js
+++
@@ -1,8 +0,0 @@
-// Auxiliar functions, these functions must not call other functions outside Wasabee
-Wasabee.aux = {};
-
-Wasabee.aux.isNonEmptyString = function(o) {
-	return typeof o == 'string' && o.trim().length > 0;
-};
-
-
--- src/code/event.js
+++
@@ -1,8 +0,0 @@
-// Event handlers, these handlers must only validate input and call controller/gui functions
-Wasabee.event = {};
-
-Wasabee.event.example = function() {
-	Wasabee.gui.exampleDialog.open();
-};
-
-
