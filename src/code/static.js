1adbc83051fa95b29e65a26c7b164f7be9634f0d
Snedecor, David <dsnedecor@gmail.com>
2019-07-10 08:29:10 -0500

    Changed import to require() for less repetition.

--- src/code/static.js
+++ src/code/static.js
@@ -1,62 +1,34 @@
-import exampleButton from "./html/exampleButton.html"
-import exampleDialog from "./html/exampleDialog.html"
-import stylesCss from "./css/styles.css"
-import toolbar_addlinks from "./images/toolbar_addlinks.png"
-import toolbar_viewOps from "./images/toolbar_viewOps.png"
-import toolbar_addMarkers from "./images/toolbar_addMarkers.png"
-import toolbar_sync from "./images/toolbar_sync.png"
-import toolbar_upload from "./images/toolbar_upload.png"
-import toolbar_download from "./images/toolbar_download.png"
-import toolbar_delete from "./images/toolbar_delete.png"
-import toolbar_plus from "./images/toolbar_plus.png"
-import toolbar_settings from "./images/toolbar_settings.png"
-import marker_layer_main from "./images/marker_layer_main.png"
-import marker_layer_groupa from "./images/marker_layer_groupa.png"
-import marker_layer_groupb from "./images/marker_layer_groupb.png"
-import marker_layer_groupc from "./images/marker_layer_groupc.png"
-import marker_layer_groupd from "./images/marker_layer_groupd.png"
-import marker_layer_groupe from "./images/marker_layer_groupe.png"
-import marker_layer_groupf from "./images/marker_layer_groupf.png"
-import marker_alert_decay from "./images/marker_alert_decay.png"
-import marker_alert_destroy from "./images/marker_alert_destroy.png"
-import marker_alert_farm from "./images/marker_alert_farm.png"
-import marker_alert_goto from "./images/marker_alert_goto.png"
-import marker_alert_link from "./images/marker_alert_link.png"
-import marker_alert_unknown from "./images/marker_alert_unknown.png"
-import marker_alert_upgrade from "./images/marker_alert_upgrade.png"
-import marker_alert_virus from "./images/marker_alert_virus.png"
-
 (Wasabee || {}).static = {
     html: {
-        exampleButton: exampleButton,
-        exampleDialog: exampleDialog,
+        exampleButton: require("./html/exampleButton.html"),
+        exampleDialog: require("./html/exampleDialog.html"),
     },
-    css: stylesCss,
+    css: require("./css/styles.css"),
     images: {
-        toolbar_addlinks: toolbar_addlinks,
-        toolbar_viewOps: toolbar_viewOps,
-        toolbar_addMarkers: toolbar_addMarkers,
-        toolbar_sync: toolbar_sync,
-        toolbar_upload: toolbar_upload,
-        toolbar_download: toolbar_download,
-        toolbar_delete: toolbar_delete,
-        toolbar_plus: toolbar_plus,
-        toolbar_settings: toolbar_settings,
-        marker_layer_main: marker_layer_main,
-        marker_layer_groupa: marker_layer_groupa,
-        marker_layer_groupb: marker_layer_groupb,
-        marker_layer_groupc: marker_layer_groupc,
-        marker_layer_groupd: marker_layer_groupd,
-        marker_layer_groupe: marker_layer_groupe,
-        marker_layer_groupf: marker_layer_groupf,
-        marker_alert_decay: marker_alert_decay,
-        marker_alert_destroy: marker_alert_destroy,
-        marker_alert_farm: marker_alert_farm,
-        marker_alert_goto: marker_alert_goto,
-        marker_alert_link: marker_alert_link,
-        marker_alert_unknown: marker_alert_unknown,
-        marker_alert_upgrade: marker_alert_upgrade,
-        marker_alert_virus: marker_alert_virus,
+        toolbar_addlinks: require("./images/toolbar_addlinks.png"),
+        toolbar_viewOps: require("./images/toolbar_viewOps.png"),
+        toolbar_addMarkers: require("./images/toolbar_addMarkers.png"),
+        toolbar_sync: require("./images/toolbar_sync.png"),
+        toolbar_upload: require("./images/toolbar_upload.png"),
+        toolbar_download: require("./images/toolbar_download.png"),
+        toolbar_delete: require("./images/toolbar_delete.png"),
+        toolbar_plus: require("./images/toolbar_plus.png"),
+        toolbar_settings: require("./images/toolbar_settings.png"),
+        marker_layer_main: require("./images/marker_layer_main.png"),
+        marker_layer_groupa: require("./images/marker_layer_groupa.png"),
+        marker_layer_groupb: require("./images/marker_layer_groupb.png"),
+        marker_layer_groupc: require("./images/marker_layer_groupc.png"),
+        marker_layer_groupd: require("./images/marker_layer_groupd.png"),
+        marker_layer_groupe: require("./images/marker_layer_groupe.png"),
+        marker_layer_groupf: require("./images/marker_layer_groupf.png"),
+        marker_alert_decay: require("./images/marker_alert_decay.png"),
+        marker_alert_destroy: require("./images/marker_alert_destroy.png"),
+        marker_alert_farm: require("./images/marker_alert_farm.png"),
+        marker_alert_goto: require("./images/marker_alert_goto.png"),
+        marker_alert_link: require("./images/marker_alert_link.png"),
+        marker_alert_unknown: require("./images/marker_alert_unknown.png"),
+        marker_alert_upgrade: require("./images/marker_alert_upgrade.png"),
+        marker_alert_virus: require("./images/marker_alert_virus.png"),
     }
     // Other static data, like: LocalStorage keys, encoded images, texts etc.
 };
