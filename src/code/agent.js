c0934e20e283fce1108b432c4683fd4fc2b7a51b
Scot Bontrager <scot@indievisible.org>
2019-07-25 21:26:06 -0500

    use standard

--- src/code/agent.js
+++ src/code/agent.js
@@ -1,27 +1,27 @@
 export default class Agent {
-    constructor() {
-        this.id = null;
-        this.name = null;
-        this.lat = 0;
-        this.lng = 0;
-        this.date = null;
-        this.pic = null;
-        this.cansendto = false;
-	this.battery = 0;
-	this.alt = 0;
-    }
+  constructor () {
+    this.id = null
+    this.name = null
+    this.lat = 0
+    this.lng = 0
+    this.date = null
+    this.pic = null
+    this.cansendto = false
+    this.battery = 0
+    this.alt = 0
+  }
 
-    static create(obj) {
-        var agent = new Agent();
-        for (var prop in obj) {
-            if (agent.hasOwnProperty(prop)) {
-                agent[prop] = obj[prop];
-            }
-	    if (prop == "OwnTracks") {
-		agent.battery = obj[prop].batt;
-		agent.alt = obj[prop].alt;
-	    }
-        }
-        return agent;
+  static create (obj) {
+    var agent = new Agent()
+    for (var prop in obj) {
+      if (agent.hasOwnProperty(prop)) {
+        agent[prop] = obj[prop]
+      }
+      if (prop === 'OwnTracks') {
+        agent.battery = obj[prop].batt
+        agent.alt = obj[prop].alt
+      }
     }
+    return agent
+  }
 }
