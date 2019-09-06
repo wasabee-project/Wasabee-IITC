7cf77578e4f817837b7f101b4e2fa1c7354fbf8c
Scot Bontrager <scot@indievisible.org>
2019-09-06 12:46:56 -0500

    fix header file

--- src/headers.js
+++ src/headers.js
@@ -1,19 +1,12 @@
 // ==UserScript==
-// @id           wasabee
-// @name         IITC Plugin: Wasabee Draw Tools
-// @namespace    http://tampermonkey.net/
-// @version      0.12.0
-// @updateURL    http://wasabee.rocks/wasabee.meta.js
-// @downloadURL  http:/wasabee.rocks/wasabee.user.js
-// @description  Less terrible draw tools, hopefully.
-// @author       Wasabee
-// @include      https://*.ingress.com/*
-// @include      http://*.ingress.com/*
-// @match        https://*.ingress.com/*
-// @match        http://*.ingress.com/*
-// @include      https://*.ingress.com/mission/*
-// @include      http://*.ingress.com/mission/*
-// @match        https://*.ingress.com/mission/*
-// @match        http://*.ingress.com/mission/*
+// @id           wasabee.user.js
+// @name         IITC plugin: Wasabee Draw Tools
+// @namespace    https://wasabee.rocks/
+// @version      0.12.1
+// @updateURL    https://wasabee.rocks/wasabee.meta.js
+// @downloadURL  https://wasabee.rocks/wasabee.user.js
+// @description  Wasabee is not for dating.
+// @author       Wasabee Project Team
+// @include      https://intel.ingress.com/*
 // @grant        none
 // ==/UserScript==
