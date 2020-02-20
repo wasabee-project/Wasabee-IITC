023b7fbe9d44bf2d689ee94100dace0433ae875b
Scot Bontrager <scot@indievisible.org>
2020-02-19 22:32:42 -0600

    disable for now, seems to cause problems

--- src/code/crosslinks.js
+++ src/code/crosslinks.js
@@ -229,7 +229,8 @@ const showCrossLink = (link, operation) => {
 
 const testLink = (link, operation) => {
   // if crosslinks are not displayed, do not check
-  if (!window.isLayerGroupDisplayed("Wasabee Cross Links")) return;
+  // requires the user to disable and enable the layer once...
+  // if (!window.isLayerGroupDisplayed("Wasabee Cross Links")) return;
 
   // if the crosslink already exists, do not recheck
   if (window.plugin.wasabee.crossLinkLayerGroup[link.options.guid]) {
