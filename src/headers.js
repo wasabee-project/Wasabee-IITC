a704dda8fd20ddaeac7ae760d9e8959de246d04f
Scot Bontrager <scot@indievisible.org>
2020-02-21 12:33:16 -0600

    bump

--- src/headers.js
+++ src/headers.js
@@ -2,9 +2,9 @@
 // @id           wasabee.user.js
 // @name         IITC plugin: Wasabee Draw Tools
 // @namespace    https://wasabee.rocks/
-// @version      0.14.2-deviousness
-// @updateURL    https://wasabee.rocks/wasabee.meta.js
-// @downloadURL  https://wasabee.rocks/wasabee.user.js
+// @version      0.14.15-deviousness
+// @updateURL    https://server.wasabee.rocks/static/dev/wasabee.meta.js
+// @downloadURL  https://server.wasabee.rocks/static/dev/wasabee.user.js
 // @description  Wasabee is not for dating.
 // @author       Wasabee Project Team
 // @include      https://intel.ingress.com/*
