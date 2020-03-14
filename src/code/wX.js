79d743c85daff4303afabc46b2fd48f3e8567d04
Scot Bontrager <scot@indievisible.org>
2020-03-14 12:33:28 -0500

    this didn't work on iOS, and upon a fraction of a second of reflection, it was a stupid way of solving the problem; this is much easier

--- src/code/wX.js
+++ src/code/wX.js
@@ -1,31 +1,17 @@
 const wT = window.plugin.wasabee.static.strings;
 
-const wX = (key, arg, option) => {
+const wX = (key, value, option) => {
   const ll = window.navigator.userLanguage || window.navigator.language || "en";
   let l = ll.substring(0, 2);
 
   // if chosen langauge does not exist, use english
   if (!wT[l]) l = "en";
-  const s = wT[l][key] || wT["en"][key];
+  let s = wT[l][key] || wT["en"][key];
 
   if (!s) return "haec notificatio praebibo est";
-
-  if (!arg) return s; // simple string, no magic needed
-
-  // one parameter
-  if (!option) {
-    const t = assemble(s, "value");
-    return t(arg);
-  }
-
-  // two parameters
-  const t = assemble(s, "value", "option");
-  return t(arg, option);
+  if (option) s = s.replace("${option}", option);
+  if (value) s = s.replace("${value}", value);
+  return s;
 };
-
-// wow this is ugly -- but it works
-function assemble(s, value, option) {
-  return new Function(value, option, "return `" + s + "`;");
-}
 
 export default wX;
