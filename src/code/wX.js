5f28e7d99c7515ca53302ed359ab1ee2fd67a768
Scot Bontrager <scot@indievisible.org>
2020-04-05 18:14:08 -0500

    use short variables to make this easier to read

--- src/code/wX.js
+++ src/code/wX.js
@@ -1,6 +1,8 @@
 // aliases to make review easier
 const strings = window.plugin.wasabee.static.strings;
 const silly = window.plugin.wasabee.static.stringsSilly;
+const defaultLang = window.plugin.wasabee.static.constants.DEFAULT_LANGUAGE;
+const localStoreKey = window.plugin.wasabee.static.constants.LANGUAGE_KEY;
 
 export const wX = (key, value, option) => {
   const lang = getLanguage();
@@ -8,12 +10,8 @@ export const wX = (key, value, option) => {
   let s = null;
   if (strings[lang] && strings[lang][key]) s = strings[lang][key];
   if (!s && silly[lang] && silly[lang][key]) s = silly[lang][key];
-  if (
-    !s &&
-    strings[window.plugin.wasabee.static.constants.DEFAULT_LANGUAGE] &&
-    strings[window.plugin.wasabee.static.constants.DEFAULT_LANGUAGE][key]
-  )
-    s = strings[window.plugin.wasabee.static.constants.DEFAULT_LANGUAGE][key];
+  if (!s && strings[defaultLang] && strings[defaultLang][key])
+    s = strings[defaultLang][key];
   if (!s) s = "haec notificatio praebibo est";
 
   // do any necessary replacements
@@ -24,17 +22,17 @@ export const wX = (key, value, option) => {
 
 export const getLanguage = () => {
   // load the selected language, or use DEFAULT_LANGUAGE if not set
-  let lang = localStorage[window.plugin.wasabee.static.constants.LANGUAGE_KEY];
+  let lang = localStorage[localStoreKey];
   if (!lang) {
-    lang = window.plugin.wasabee.static.constants.DEFAULT_LANGUAGE;
-    localStorage[window.plugin.wasabee.static.constants.LANGUAGE_KEY] = lang;
+    lang = defaultLang;
+    localStorage[localStoreKey] = defaultLang;
     console.log("no language set, using default");
   }
 
   // if the langauge doesn't exist in either list, clear it and use DEFAULT_LANGUAGE
   if (!strings[lang] && !silly[lang]) {
-    lang = window.plugin.wasabee.static.constants.DEFAULT_LANGUAGE;
-    localStorage[window.plugin.wasabee.static.constants.LANGUAGE_KEY] = lang;
+    lang = defaultLang;
+    localStorage[localStoreKey] = defaultLang;
     console.log("invalid language set, changing to default");
   }
 
