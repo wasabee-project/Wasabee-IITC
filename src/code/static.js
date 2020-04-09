6ec2c064ef6dd18807a2bcc7f224e0fe97c6947d
Scot Bontrager <scot@indievisible.org>
2020-04-09 10:37:15 -0500

    remove unused

--- src/code/static.js
+++ src/code/static.js
@@ -12,15 +12,12 @@ W.static = {
     toolbar_sync: require("./images/toolbar_sync.png"),
     toolbar_upload: require("./images/toolbar_upload.png"),
     toolbar_download: require("./images/toolbar_download.png"),
-    toolbar_delete: require("./images/toolbar_delete.png"),
-    toolbar_plus: require("./images/toolbar_plus.png"),
     toolbar_wasabeebutton_in: require("./images/toolbar_wasabeebutton_in.png"),
     toolbar_wasabeebutton_out: require("./images/toolbar_wasabeebutton_out.png"),
     toolbar_wasabeebutton_se: require("./images/toolbar_wasabeebutton_se.png"),
     toolbar_wasabeebutton_seg: require("./images/toolbar_wasabeebutton_seg.png"),
     toolbar_quickdraw: require("./images/toolbar_quickdraw.png"),
-    toolbar_settings: require("./images/toolbar_settings.png"),
-    toolbar_multimax: require("./images/toolbar_multimax.png")
+    toolbar_settings: require("./images/toolbar_settings.png")
   },
   dialogNames: {
     linkDialogButton: "wasabee-addlinks",
@@ -66,8 +63,7 @@ W.static = {
     MARKER_TYPE_UPGRADE: "UpgradePortalAlert",
     MARKER_TYPE_VIRUS: "UseVirusPortalAlert",
     MARKER_TYPE_EXCLUDE: "ExcludeMarker",
-    DEFAULT_MARKER_TYPE: "DestroyPortalAlert",
-    SCRIPT_URL_NOTY: "http://wasabee.rocks/wasabee_extras/noty.js"
+    DEFAULT_MARKER_TYPE: "DestroyPortalAlert"
   }
 };
 
