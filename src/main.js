7cc6e2925542ac63c69d6f87544db18826af66cb
Scot Bontrager <scot@indievisible.org>
2020-07-13 09:46:53 -0500

    revert, this broke on many platforms

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@
 function wrapper(plugin_info) {
   /* inject: ./wrapper/pluginStart.js				*/
 
-  window.plugin.wasabee = {};
+  window.plugin.Wasabee = window.plugin.wasabee = {};
 
   // Code injection
   let setup = function() {
     /* inject: ../dist/static-bundle.js                     */
+
     /* inject: ../dist/init-bundle.js                     */
 
     window.plugin.wasabee.init();
