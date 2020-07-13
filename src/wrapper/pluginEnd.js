8c8dee2dcd82d4211cd76bbfc879bbf7e39ae618
Scot Bontrager <scot@indievisible.org>
2020-07-13 09:22:12 -0500

    revert

--- src/wrapper/pluginEnd.js
+++ src/wrapper/pluginEnd.js
@@ -1,6 +1,10 @@
 // PLUGIN END ----------------------------------------------------------
 setup.info = plugin_info; //add the script info data to the function as a property
-if (!window.bootPlugins) window.bootPlugins = [];
+if (!window.bootPlugins) {
+  window.bootPlugins = [];
+}
 window.bootPlugins.push(setup);
 // if IITC has already booted, immediately run the 'setup' function
-if (window.iitcLoaded && typeof setup === "function") setup();
+if (window.iitcLoaded && typeof setup === "function") {
+  setup();
+}
--- src/wrapper/pluginStart.js
+++ src/wrapper/pluginStart.js
@@ -1,3 +1,6 @@
 // ensure plugin framework is there, even if iitc is not yet loaded
-if (typeof window.plugin !== "function") window.plugin = () => {};
+if (typeof window.plugin !== "function") {
+  window.plugin = function() {};
+}
+
 // PLUGIN START --------------------------------------------------------
