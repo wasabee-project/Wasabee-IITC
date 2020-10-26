b2f6954c8256880476e7a98bf23076a197a10f5e
Scot Bontrager <scot@indievisible.org>
2020-10-26 16:27:26 -0500

    bring up-to-date

--- src/code/agent.js
+++ src/code/agent.js
@@ -17,20 +17,28 @@ export default class WasabeeAgent {
 
     this.id = obj.id;
     this.name = obj.name; // XXX gets messy in the cache if team display name is set
-    this.lat = obj.lat ? obj.lat : 0;
-    this.lng = obj.lng ? obj.lng : 0;
-    this.date = obj.date ? obj.date : null;
+    this.level = obj.level ? Number(obj.level) : 0;
+    this.enlid = obj.enlid ? obj.enlid : 0;
     this.pic = obj.pic ? obj.pic : null;
-    this.cansendto = obj.cansendto ? obj.cansendto : false;
     this.Vverified = obj.Vverified ? obj.Vverified : false;
     this.blacklisted = obj.blacklisted ? obj.blacklisted : false;
     this.rocks = obj.rocks ? obj.rocks : false;
-
-    // squad and state are meaningless in the cache since you can never know which team set them
-    this.squad = obj.squad ? obj.squad : null;
-    this.state = obj.state;
+    this.lat = obj.lat ? obj.lat : 0;
+    this.lng = obj.lng ? obj.lng : 0;
+    this.date = obj.date ? obj.date : null;
+    this.cansendto = obj.cansendto ? obj.cansendto : false; // never true from a team pull
     this.ShareWD = obj.ShareWD;
     this.LoadWD = obj.LoadWD;
+    this.startlat = obj.startlat ? obj.startlat : 0;
+    this.startlng = obj.startlng ? obj.startlng : 0;
+    this.startradius = obj.startradius ? Number(obj.startradius) : 0;
+    this.sharestart = obj.sharestart ? obj.sharestart : false;
+
+    // distance, dispayname, squad and state are meaningless in the cache since you cannot know which team set them
+    this.distance = obj.distance ? Number(obj.distance) : 0; // don't use this
+    this.displayname = obj.displayname ? obj.displayname : "";
+    this.squad = obj.squad ? obj.squad : null;
+    this.state = obj.state;
 
     // push the new data into the agent cache
     window.plugin.wasabee._agentCache.set(this.id, this);
