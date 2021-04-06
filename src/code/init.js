a5f5d4e2459fb87bf3c71d9913e81bb99b139cfc
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-04-06 21:28:50 +0200

    handle db version upgrade

--- src/code/init.js
+++ src/code/init.js
@@ -169,31 +169,34 @@ function initGoogleAPI() {
 }
 
 async function initIdb() {
-  const version = 1;
+  const version = 2;
   // if (Wasabee.idb && Wasabee.idb.version == version) return;
 
   // XXX audit these to make sure all the various indexes are used
   Wasabee.idb = await openDB("wasabee", version, {
-    upgrade(db) {
-      const agents = db.createObjectStore("agents", { keyPath: "id" });
-      agents.createIndex("date", "date"); // last location change
-      agents.createIndex("fetched", "fetched"); // last pull from server
-      const teams = db.createObjectStore("teams", { keyPath: "id" });
-      teams.createIndex("fetched", "fetched"); // last pull from server
+    upgrade(db, oldVersion) {
+      if (oldVersion < 1) {
+        const agents = db.createObjectStore("agents", { keyPath: "id" });
+        agents.createIndex("date", "date"); // last location change
+        agents.createIndex("fetched", "fetched"); // last pull from server
+        const teams = db.createObjectStore("teams", { keyPath: "id" });
+        teams.createIndex("fetched", "fetched"); // last pull from server
 
-      // do not set an implied key, explicitly set GID/PortalID on insert
-      // XXX we can do this with a keyPath https://stackoverflow.com/questions/33852508/how-to-create-an-indexeddb-composite-key
-      // const defensivekeys = db.createObjectStore("defensivekeys");
-      const defensivekeys = db.createObjectStore("defensivekeys", {
-        keyPath: ["GID", "PortalID"],
-      });
-      defensivekeys.createIndex("PortalID", "PortalID");
-      defensivekeys.createIndex("Count", "Count"); // To be used to remove 0-count entries
-      // defensivekeys.createIndex("pk", ["GID", "PortalID"], { unique: true });
-      //
-      const ops = db.createObjectStore("operations", { keyPath: "ID" });
-      ops.createIndex("fetched", "fetched");
-      ops.createIndex("server", "server");
+        // do not set an implied key, explicitly set GID/PortalID on insert
+        // XXX we can do this with a keyPath https://stackoverflow.com/questions/33852508/how-to-create-an-indexeddb-composite-key
+        // const defensivekeys = db.createObjectStore("defensivekeys");
+        const defensivekeys = db.createObjectStore("defensivekeys", {
+          keyPath: ["GID", "PortalID"],
+        });
+        defensivekeys.createIndex("PortalID", "PortalID");
+        defensivekeys.createIndex("Count", "Count"); // To be used to remove 0-count entries
+        // defensivekeys.createIndex("pk", ["GID", "PortalID"], { unique: true });
+      }
+      if (oldVersion < 2) {
+        const ops = db.createObjectStore("operations", { keyPath: "ID" });
+        ops.createIndex("fetched", "fetched");
+        ops.createIndex("server", "server");
+      }
     },
   });
 }
