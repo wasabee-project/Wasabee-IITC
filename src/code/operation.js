0bcc8c58c45f0f582581d231073f892662def18c
Scot Bontrager <scot@indievisible.org>
2021-05-16 18:36:14 -0500

    prettier

--- src/code/operation.js
+++ src/code/operation.js
@@ -867,7 +867,11 @@ export default class WasabeeOp {
 
   runCrosslinks() {
     if (this._batchmode === true) return;
-    window.map.fire("wasabee:crosslinks", { reason: "op runCrosslinks" }, false);
+    window.map.fire(
+      "wasabee:crosslinks",
+      { reason: "op runCrosslinks" },
+      false
+    );
   }
 
   startBatchMode() {
