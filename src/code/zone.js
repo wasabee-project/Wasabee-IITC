a4bd62d410501a7554ea176da2b71bf8edc81f5e
Scot Bontrager <scot@indievisible.org>
2021-05-16 20:22:37 -0500

    need to commit before I rebase

--- src/code/zone.js
+++ src/code/zone.js
@@ -2,12 +2,30 @@ export default class WasabeeZone {
   constructor(obj) {
     this.id = Number(obj.id);
     this.name = obj.name;
+    this.color = obj.color ? obj.color : "00ff00";
+    this.points = [];
+
+    if (obj.points) {
+      for (p of obj.points) {
+        this.points.push(new zonePoint(p));
+      }
+    }
   }
 
   toJSON() {
     return {
       id: Number(this.id),
       name: `${this.name}`,
+      color: this.color,
+      points: points,
     };
+  }
+}
+
+class zonePoint {
+  constructor(obj) {
+    this.position = Number(obj.position);
+    this.lat = Number(obj.lat);
+    this.lng = Number(obj.lng);
   }
 }
