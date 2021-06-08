e332061e5911e3f53cf6be00594a11b50b329b91
TheMitch2 <62919690+TheMitch2@users.noreply.github.com>
2021-06-08 07:49:43 -0500

    Update importDialog.js

--- src/code/dialogs/importDialog.js
+++ src/code/dialogs/importDialog.js
@@ -70,7 +70,9 @@ const ImportDialog = WDialog.extend({
   },
 
   drawToolsFormat() {
-    const dtitems = localStorage[window.plugin.drawTools.KEY_STORAGE];
+    const dtitems = window.plugin.drawTools
+      ? localStorage[window.plugin.drawTools.KEY_STORAGE]
+      : undefined;
     if (dtitems) {
       this._textarea.value = dtitems;
     } else {
