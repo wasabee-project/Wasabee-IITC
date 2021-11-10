c35c6cd6145d71672d4ea4d4d18a69be062c486b
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-11-10 22:57:00 +0100

    me.ts

--- src/code/model/me.ts
+++ src/code/model/me.ts
@@ -3,7 +3,43 @@ import db from "../db";
 
 import { constants } from "../static";
 
+export interface MeTeam {
+  ID: string,
+  Name: string,
+  RocksComm: string,
+  RocksKey: string,
+  JoinLinkToken: string,
+  ShareWD: "On" | "Off",
+  LoadWD: "On" | "Off",
+  State: "On" | "Off",
+  Owner: string,
+  VTeam: string,
+  VTeamRole: string,
+}
+
+interface MeOp {
+  ID: string,
+}
+
 export default class WasabeeMe {
+  GoogleID: string;
+  name: string;
+  vname: string;
+  rocksname: string;
+  intelname: string;
+  level: number;
+  Teams: Array<MeTeam>;
+  Ops: Array<MeOp>;
+  fetched: number;
+  Vverified: boolean;
+  blacklisted: boolean;
+  enlid: string;
+  pic: string;
+  intelfaction: string;
+  querytoken: string;
+
+  _teamMap: Map<string, "On" | "Off">;
+
   constructor(data) {
     if (typeof data == "string") {
       try {
@@ -100,7 +136,7 @@ export default class WasabeeMe {
   }
 
   // use waitGet with "force == true" if you want a fresh value now
-  static async waitGet(force) {
+  static async waitGet(force?: boolean) {
     let me = WasabeeMe.localGet();
 
     if (
