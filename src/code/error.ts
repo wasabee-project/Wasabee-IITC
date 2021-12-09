69064ae9781af8b69860427b992bf88ad56b74cd
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-12-09 22:00:09 +0100

    error: fix http code == 0

--- src/code/error.ts
+++ src/code/error.ts
@@ -8,25 +8,28 @@ interface IServerError {
 
 export function displayInfo(content: string | HTMLElement, isHTML?: boolean) {
   return window.dialog({
-    title: 'Wasabee info',
+    title: "Wasabee info",
     text: isHTML ? null : content.toString(),
     html: isHTML ? content : null,
-  })
+  });
 }
 
-export function displayWarning(content: string | HTMLElement, isHTML?: boolean) {
+export function displayWarning(
+  content: string | HTMLElement,
+  isHTML?: boolean
+) {
   return window.dialog({
-    title: 'Wasabee warning',
+    title: "Wasabee warning",
     text: isHTML ? null : content.toString(),
     html: isHTML ? content : null,
-  })
+  });
 }
 
 export function displayError(err: { toString(): string }) {
   return window.dialog({
-    title: 'Wasabee error',
+    title: "Wasabee error",
     text: err.toString(),
-  })
+  });
 }
 
 // no stacktrace error
@@ -39,30 +42,30 @@ export class ServerError implements IServerError {
     this.code = obj.code;
     this.text = obj.text;
     this.error = obj.error;
+
+    if (!this.code) {
+      this.code = -1;
+      this.text = "Unknown error";
+    }
   }
 
   toString() {
     switch (this.code) {
       case 401:
-        if (this.error)
-          return wX("NOT LOGGED IN", this);
+        if (this.error) return wX("NOT LOGGED IN", this);
         return wX("NOT LOGGED IN SHORT");
       case 403:
-        if (this.error)
-          return wX("PERM DENIED", this);
+        if (this.error) return wX("PERM DENIED", this);
         return wX("PERM DENIED SHORT");
       case 410:
-        if (this.error)
-          return wX("NO LONGER AVAILABLE", this);
+        if (this.error) return wX("NO LONGER AVAILABLE", this);
         return wX("NO LONGER AVAILABLE SHORT");
       case 412:
         // for internal use only
-        if (this.error)
-          return 'Mismatch version: ' + this.error;
-        return 'Mismatch version';
+        if (this.error) return "Mismatch version: " + this.error;
+        return "Mismatch version";
       default:
-        if (this.error)
-          return this.text + ': ' + this.error;
+        if (this.error) return this.text + ": " + this.error;
         return this.text;
     }
   }
