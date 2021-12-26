b6015b6d45cbc8f451b3bef159d37e4e3aac86b9
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-12-27 00:22:17 +0100

    update dev build to pr303 (rebased)

--- dev/wasabee.meta.js
+++ dev/wasabee.meta.js
@@ -2,7 +2,7 @@
 // @id            Wasabee.user.js
 // @name          IITC plugin: Wasabee Draw Tools
 // @namespace     https://wasabee.rocks/
-// @version       0.20.20211226143045-dev-acb9a58
+// @version       0.20.20211226232123-dev-200f2e54
 // @updateURL     https://cdn2.wasabee.rocks/iitcplugin/latest/dev/wasabee.meta.js
 // @downloadURL   https://cdn2.wasabee.rocks/iitcplugin/latest/dev/wasabee.user.js
 // @description   Wasabee is not for dating.
