2c28ce67ebb6274ce8d8e88c78a69ea50eee6e95
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-12-27 00:01:38 +0100

    onion: derecurse
    add balanced, equilateral (close to previous), "let it grow" (keep
    adding layer from equilateral)

--- src/code/dialogs/autodraws/onionfield.js
+++ src/code/dialogs/autodraws/onionfield.js
@@ -1,8 +1,7 @@
 import { AutoDraw } from "./tools";
 import WasabeePortal from "../../model/portal";
 import { getSelectedOperation } from "../../selectedOp";
-import { greatCircleArcIntersectByLatLngs } from "../../crosslinks";
-import WasabeeLink from "../../model/link";
+import { portalInField } from "../../crosslinks";
 import { clearAllLinks, getAllPortalsOnScreen } from "../../uiCommands";
 import wX from "../../wX";
 import { displayError } from "../../error";
@@ -38,6 +37,19 @@ const OnionfieldDialog = AutoDraw.extend({
       window.plugin.wasabee.static.constants.ANCHOR_ONE_KEY
     );
 
+    const optionLabel = L.DomUtil.create("label", null, container);
+    optionLabel.textContent = "Options"; // wX
+    this.optionMenu = L.DomUtil.create("select", null, container);
+    for (const [text, value] of [
+      ["~Equilateral", "equi"],
+      ["Let it grow", "grow"],
+      ["Perfect balance", "balanced"], // need wX on first column
+    ]) {
+      const option = L.DomUtil.create("option", null, this.optionMenu);
+      option.value = value;
+      option.textContent = text;
+    }
+
     // Go button
     const button = L.DomUtil.create("button", "drawb", container);
     button.textContent = wX("ONION");
@@ -69,7 +81,7 @@ const OnionfieldDialog = AutoDraw.extend({
       displayError("no anchor selected");
       return;
     }
-    this._colors = [
+    const colors = [
       "#f80c12",
       "#ee1100",
       "#ff3311",
@@ -89,210 +101,112 @@ const OnionfieldDialog = AutoDraw.extend({
       "#3b0cbd",
       "#442299",
     ];
-    this._colorIterator = 0;
-    this._color = this._colors[this._colorIterator];
     const allPortals = getAllPortalsOnScreen(this._operation);
 
-    // batch mode isn't strictly necessary since we are loading the links in one operation, but portals are added during the recursion
+    const links = this._recurser(
+      allPortals,
+      this._anchor,
+      this.optionMenu.value
+    );
+
     this._operation.startBatchMode();
-
-    // add the anchor to the operation, needed to check for crosslinks
-    this._operation.addPortal(this._anchor);
-    // start digging for onions
-    const onion = this._recurser(allPortals, [], this._anchor);
-
-    // add all the found links at once
-    // this is a minor abuse of the _operation object since we aren't using the  operation.addLink() method to add links
-    this._operation.links.push(...onion);
-    // that didn't add the anchors, so we have to do that here
-    this._operation.cleanAnchorList();
-    // now, remove the portals that are unused
-    this._operation.cleanPortalList();
-    // signal to the operation that we are done abusing it
+    links.forEach((l, i) =>
+      this._operation.addLink(l.from, l.to, {
+        order: i + 1,
+        color: colors[i % colors.length],
+      })
+    );
     this._operation.endBatchMode();
-  },
-
-  // no longer operates on a a class var, different paths (if not using the fast route) have different lists
-  _removeFromList: function (portalsRemaining, guid) {
-    const x = new Array();
-    for (const p of portalsRemaining) {
-      if (p.id != guid) x.push(p);
-    }
-    return x;
   },
 
   // the data passing for this (portalsRemaining, thisPath)
   // is designed to allow for determining optimum path, the fast route is quick
   // and gets a reasonable set, optimum path determination is VERY slow and nets
   // only a few extra layers
-  _recurser: function (portalsRemaining, thisPath, one, two, three) {
-    this._colorIterator = (this._colorIterator + 1) % this._colors.length;
-    this._color = this._colors[this._colorIterator];
+  _recurser: function (portalsRemaining, start, type) {
+    portalsRemaining = portalsRemaining.filter((p) => p.id !== start.id);
+    if (portalsRemaining.length < 2) return [];
 
-    // build a map of all portals still in-play
     const m = new Map();
     for (const p of portalsRemaining) {
-      if (
-        (two && p.id == two.id) ||
-        (three && p.id == three.id) ||
-        p.id == one.id
-      ) {
-        portalsRemaining = this._removeFromList(portalsRemaining, p.id);
-        continue;
-      }
-
-      const pDist = window.map.distance(one.latLng, p.latLng);
+      const pDist = window.map.distance(start.latLng, p.latLng);
       m.set(pDist, p);
     }
-    // sort by distance
-    const sorted = new Map([...m.entries()].sort((a, b) => a[0] - b[0]));
-    if (sorted.length == 0) return null;
+    const sorted = [...m.entries()]
+      .sort((a, b) => a[0] - b[0])
+      .map((a) => a[1]);
 
-    // for each of the portals in play
-    for (const [k, wp] of sorted) {
-      // silence lint
-      this._trash = k;
+    let [one, two, three] = this._sortAnchors(start, sorted[0], sorted[1]);
+    const path = [
+      {
+        from: two,
+        to: one,
+      },
+      {
+        from: three,
+        to: one,
+      },
+      {
+        from: three,
+        to: two,
+      },
+    ];
 
-      // we need it in the op (this prevents dupes) for links later
-      this._operation.addPortal(wp);
-      // unused ones will be purged at the end
+    portalsRemaining = portalsRemaining.filter(
+      (p) => p.id !== two.id && p.id !== three.id
+    );
 
-      // do the intial field
-      if (!two) {
-        portalsRemaining = this._removeFromList(portalsRemaining, wp.id);
-        const a = new WasabeeLink(
-          { fromPortalId: one.id, toPortalId: wp.id },
-          this._operation
-        );
-        a.color = this._color;
-        a.throwOrderPos = 1;
-        thisPath.push(a);
-        return this._recurser(portalsRemaining, thisPath, one, wp);
+    const round = type === "grow" ? 3 : 1;
+
+    let running = round;
+    while (running > 0 && portalsRemaining.length > 0) {
+      running = running - 1;
+
+      // sorted remaining by distance to one
+      const m = new Map();
+      for (const p of portalsRemaining) {
+        const pDist = window.map.distance(one.latLng, p.latLng);
+        m.set(pDist, p);
       }
-      if (!three) {
-        portalsRemaining = this._removeFromList(portalsRemaining, wp.id);
-        const a = new WasabeeLink(
-          { fromPortalId: one.id, toPortalId: wp.id },
-          this._operation
-        );
-        a.color = this._color;
-        a.throwOrderPos = 2;
-        thisPath.push(a);
-        const b = new WasabeeLink(
-          { fromPortalId: two.id, toPortalId: wp.id },
-          this._operation
-        );
-        b.color = this._color;
-        b.throwOrderPos = 3;
-        thisPath.push(b);
-        // now we are bootstrapped, dive in
-        return this._recurser(portalsRemaining, thisPath, one, two, wp);
-      }
-      // initial field done
+      const sorted = [...m.entries()]
+        .sort((a, b) => a[0] - b[0])
+        .map((a) => a[1]);
 
-      // create the three links, this does not add them to the operation
-      const a = new WasabeeLink(
-        { fromPortalId: one.id, toPortalId: wp.id },
-        this._operation
-      );
-      const b = new WasabeeLink(
-        { fromPortalId: two.id, toPortalId: wp.id },
-        this._operation
-      );
-      const c = new WasabeeLink(
-        { fromPortalId: three.id, toPortalId: wp.id },
-        this._operation
-      );
-      a.color = this._color;
-      b.color = this._color;
-      c.color = this._color;
+      // for each of the portals in play
+      const wp = sorted.find((p) => portalInField(p, two, three, one));
+      if (wp) {
+        running = round;
+        portalsRemaining = portalsRemaining.filter((p) => p.id !== wp.id);
 
-      // testBlock does not look in the op or live map data, but in thisPath
-      const aBlock = this._testBlock(thisPath, a);
-      const bBlock = this._testBlock(thisPath, b);
-      const cBlock = this._testBlock(thisPath, c);
+        path.push({ from: wp, to: two });
+        path.push({ from: wp, to: three });
+        path.push({ from: wp, to: one });
 
-      // if none of the links are blocked by existing linkes in thisPath, we found an option
-      if (!aBlock && !bBlock && !cBlock) {
-        portalsRemaining = this._removeFromList(portalsRemaining, wp.id);
-
-        let Y = one;
-        let Z = two;
-        // determine the widest angle, wp<YZ, use that
-        const angOneTwo = this._angle(wp, one, two);
-        const angTwoThree = this._angle(wp, two, three);
-        const angThreeOne = this._angle(wp, three, one);
-        if (angOneTwo >= angTwoThree && angOneTwo >= angThreeOne) {
-          Y = one;
-          Z = two;
-          thisPath.push(a);
-          a.throwOrderPos = thisPath.length;
-          thisPath.push(b);
-          b.throwOrderPos = thisPath.length;
-          // XXX add back-link to c if requested
-        } else if (angTwoThree >= angThreeOne) {
-          Y = two;
-          Z = three;
-          thisPath.push(b);
-          b.throwOrderPos = thisPath.length;
-          thisPath.push(c);
-          c.throwOrderPos = thisPath.length;
-          // XXX add back-link to a if requested
+        if (type === "balanced") {
+          [one, two, three] = [two, three, wp];
         } else {
-          Y = three;
-          Z = one;
-          thisPath.push(c);
-          c.throwOrderPos = thisPath.length;
-          thisPath.push(a);
-          a.throwOrderPos = thisPath.length;
-          // XXX add back-link to b if requested
+          // determine the widest angle, wp<23, use that to determine next covered portal
+          [one, two, three] = this._sortAnchors(two, three, wp);
         }
-
-        // allow users to turn this on if they want...
-        if (
-          localStorage["wasabee-onion-recursive"] &&
-          localStorage["wasabee-onion-recursive"] == "slowAF"
-        ) {
-          // you might find 2 or 3 more layers on a small field, but it takes an hour to run
-          // two is enough, three gets crazy even with a small number of portals
-          const pathOne = this._recurser(
-            new Array(...portalsRemaining),
-            new Array(...thisPath),
-            Y,
-            Z,
-            wp
-          );
-          const pathTwo = this._recurser(
-            new Array(...portalsRemaining),
-            new Array(...thisPath),
-            Z,
-            Y,
-            wp
-          );
-          // console.log(pathOne.length, pathTwo.length);
-          if (pathTwo.length > pathOne.lenght) return pathTwo;
-          return pathOne;
+      }
+      if (running < round) {
+        if (type == "grow") {
+          // drop the widest angle constraint, take the next one
+          [one, two, three] = [two, three, one];
         }
-        // default to the fast route, which gets gets us 90+% in my testing
-        return this._recurser(portalsRemaining, thisPath, Y, Z, wp);
       }
     }
     // console.log("hit bottom", thisPath.length);
-    return thisPath;
+    return path;
   },
 
-  // looks only at links in current (not op or live data)
-  _testBlock: function (current, testing) {
-    for (const against of current) {
-      if (
-        greatCircleArcIntersectByLatLngs(
-          testing.getLatLngs(this._operation),
-          against.getLatLngs(this._operation)
-        )
-      )
-        return true;
-    }
-    return false;
+  _sortAnchors: function (one, two, three) {
+    const a = [
+      [one, this._angle(one, two, three)],
+      [two, this._angle(two, three, one)],
+      [three, this._angle(three, one, two)],
+    ];
+    return a.sort((a, b) => b[1] - a[1]).map((a) => a[0]);
   },
 
   // angle a<bc in radians
