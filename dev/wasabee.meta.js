6afa6b780f85d1e3e6ba25241e003cbf397f6658
SebastienForay <contact@sebastienforay.fr>
2022-01-17 01:14:15 +0000

    Deploying to dist from @ wasabee-project/Wasabee-IITC@eae1a043976a346dd47046759771aed3131555e4 🚀

--- dev/wasabee.meta.js
+++ dev/wasabee.meta.js
@@ -2,7 +2,7 @@
 // @id            Wasabee.user.js
 // @name          IITC plugin: Wasabee Draw Tools
 // @namespace     https://wasabee.rocks/
-// @version       0.20.20220116222905-dev-6d49d41
+// @version       0.20.20220117011357-dev-eae1a04
 // @updateURL     https://cdn2.wasabee.rocks/iitcplugin/latest/dev/wasabee.meta.js
 // @downloadURL   https://cdn2.wasabee.rocks/iitcplugin/latest/dev/wasabee.user.js
 // @description   Wasabee is not for dating.
