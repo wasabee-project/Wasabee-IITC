4100bc3cc54615315cc195a9a7b9284cbc3607a5
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-01-20 11:27:03 +0100

    perms: align adding form with the table

--- src/code/dialogs/opPerms.js
+++ src/code/dialogs/opPerms.js
@@ -48,13 +48,14 @@ const OpPermList = WDialog.extend({
     const html = this.buildHTML(operation);
 
     const buttons = {};
-    buttons[wX("OK")] = () => {
+    buttons[wX("CLOSE")] = () => {
       this.closeDialog();
     };
 
     this.createDialog({
       title: wX("PERMS", { opName: operation.name }),
       html: html,
+      width: "auto",
       height: "auto",
       dialogClass: "perms",
       buttons: buttons,
@@ -62,59 +63,8 @@ const OpPermList = WDialog.extend({
   },
 
   buildHTML: function (operation) {
-    const html = L.DomUtil.create("div");
-    html.appendChild(this.buildTable(operation));
-
-    if (this._me && operation.isOwnedOp() && operation.isOnCurrentServer()) {
-      const already = new Set();
-      for (const a of operation.teamlist) already.add(a.teamid);
-
-      const addArea = L.DomUtil.create("div", "add-perm", html);
-      const teamMenu = L.DomUtil.create("select", null, addArea);
-      for (const t of this._me.Teams) {
-        // if (already.has(t.ID)) continue;
-        const o = L.DomUtil.create("option", null, teamMenu);
-        o.value = t.ID;
-        o.textContent = t.Name;
-      }
-      const permMenu = L.DomUtil.create("select", null, addArea);
-      const read = L.DomUtil.create("option", null, permMenu);
-      read.value = "read";
-      read.textContent = wX("READ");
-      const write = L.DomUtil.create("option", null, permMenu);
-      write.value = "write";
-      write.textContent = wX("WRITE");
-      const ao = L.DomUtil.create("option", null, permMenu);
-      ao.value = "assignedonly";
-      ao.textContent = wX("ASSIGNED_ONLY");
-
-      const zoneMenu = L.DomUtil.create("select", null, addArea);
-      const zoneAll = L.DomUtil.create("option", null, zoneMenu);
-      zoneAll.value = "0";
-      zoneAll.textContent = "All";
-      for (const oz of operation.zones) {
-        const z = L.DomUtil.create("option", null, zoneMenu);
-        z.value = oz.id;
-        z.textContent = oz.name;
-      }
-
-      const ab = L.DomUtil.create("button", null, addArea);
-      ab.type = "button";
-      ab.name = "Add";
-      ab.value = "Add";
-      ab.textContent = wX("ADD");
-
-      L.DomEvent.on(ab, "click", (ev) => {
-        L.DomEvent.stop(ev);
-        this.addPerm(teamMenu.value, permMenu.value, +zoneMenu.value); // async, but no need to await
-        // addPerm calls wasabee:op:change, which redraws the screen
-      });
-    }
-
-    return html;
-  },
-
-  buildTable: function (operation) {
+    const isOwner =
+      this._me && operation.isOwnedOp() && operation.isOnCurrentServer();
     const sortable = new Sortable();
     const fields = [
       {
@@ -136,24 +86,57 @@ const OpPermList = WDialog.extend({
         },
         sort: (a, b) => a.localeCompare(b),
         // , format: (cell, value) => (cell.textContent = value)
+        foot: (cell) => {
+          this.teamMenu = L.DomUtil.create("select", null, cell);
+          for (const t of this._me.Teams) {
+            const o = L.DomUtil.create("option", null, this.teamMenu);
+            o.value = t.ID;
+            o.textContent = t.Name;
+          }
+        },
       },
       {
         name: wX("ROLE"),
         value: (perm) => perm.role,
         sort: (a, b) => a.localeCompare(b),
         // , format: (cell, value) => (cell.textContent = value)
+        foot: (cell) => {
+          this.permMenu = L.DomUtil.create("select", null, cell);
+          const read = L.DomUtil.create("option", null, this.permMenu);
+          read.value = "read";
+          read.textContent = wX("READ");
+          const write = L.DomUtil.create("option", null, this.permMenu);
+          write.value = "write";
+          write.textContent = wX("WRITE");
+          const ao = L.DomUtil.create("option", null, this.permMenu);
+          ao.value = "assignedonly";
+          ao.textContent = wX("ASSIGNED_ONLY");
+        },
       },
       {
         name: "Zone",
         value: (perm) => operation.zoneName(perm.zone),
         sort: (a, b) => a.localeCompare(b),
         // , format: (cell, value) => (cell.textContent = value)
+        foot: (cell) => {
+          this.zoneMenu = L.DomUtil.create("select", null, cell);
+          const zoneAll = L.DomUtil.create("option", null, this.zoneMenu);
+          zoneAll.value = "0";
+          zoneAll.textContent = "All";
+          for (const oz of operation.zones) {
+            const z = L.DomUtil.create("option", null, this.zoneMenu);
+            z.value = oz.id;
+            z.textContent = oz.name;
+          }
+        },
       },
     ];
 
-    if (operation.isOwnedOp()) {
+    if (!isOwner) {
+      for (const field of fields) delete field.foot;
+    } else {
       fields.push({
-        name: wX("REMOVE"),
+        name: "Commands",
         value: () => wX("REMOVE"),
         format: (cell, value, obj) => {
           const link = L.DomUtil.create("a", null, cell);
@@ -162,6 +145,19 @@ const OpPermList = WDialog.extend({
           L.DomEvent.on(link, "click", (ev) => {
             L.DomEvent.stop(ev);
             this.delPerm(obj); // calls wasabee:op:change -- async but no need to await
+          });
+        },
+        foot: (cell) => {
+          const link = L.DomUtil.create("a", null, cell);
+          link.href = "#";
+          link.textContent = wX("ADD");
+          L.DomEvent.on(link, "click", (ev) => {
+            L.DomEvent.stop(ev);
+            this.addPerm(
+              this.teamMenu.value,
+              this.permMenu.value,
+              +this.zoneMenu.value
+            );
           });
         },
       });
