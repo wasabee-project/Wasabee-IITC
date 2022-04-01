e6bca08881abd4ec76e183d0acdc565dc1536811
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-04-01 21:57:06 +0200

    fix: assign all outbound links default agent

--- src/code/dialogs/assignDialog.js
+++ src/code/dialogs/assignDialog.js
@@ -26,6 +26,7 @@ const AssignDialog = WDialog.extend({
   _displayDialog: function () {
     const buttons = {};
     buttons[wX("OK")] = () => {
+      this.localAssign(this._currentAssign);
       this.closeDialog();
     };
 
@@ -46,6 +47,8 @@ const AssignDialog = WDialog.extend({
 
     const target = this.options.target;
     const operation = getSelectedOperation();
+    this._currentAssign = null;
+
     this._targetID = target.ID;
 
     const divtitle = L.DomUtil.create("div", "desc", html);
@@ -60,6 +63,7 @@ const AssignDialog = WDialog.extend({
       divtitle.appendChild(LinkUI.displayFormat(target, operation));
       const t = L.DomUtil.create("label", null, menu);
       t.textContent = wX("LINK ASSIGNMENT");
+      this._currentAssign = target.assignedTo;
     }
 
     if (target instanceof WasabeeMarker) {
@@ -71,6 +75,7 @@ const AssignDialog = WDialog.extend({
       divtitle.appendChild(PortalUI.displayFormat(portal));
       const t = L.DomUtil.create("label", null, menu);
       t.textContent = wX("MARKER ASSIGNMENT");
+      this._currentAssign = target.assignedTo;
     }
 
     if (target instanceof WasabeePortal) {
@@ -82,6 +87,12 @@ const AssignDialog = WDialog.extend({
       divtitle.appendChild(PortalUI.displayFormat(portal));
       const t = L.DomUtil.create("label", null, menu);
       t.textContent = wX("ANCHOR ASSIGNMENT");
+      for (const l of operation.getLinkListFromPortal(portal)) {
+        if (l.fromPortalId === portal.id && l.assignedTo) {
+          this._currentAssign = l.assignedTo;
+          break;
+        }
+      }
     }
 
     const select = L.DomUtil.create("select", null, menu);
@@ -90,10 +101,11 @@ const AssignDialog = WDialog.extend({
     option.textContent = wX("UNASSIGNED");
 
     L.DomEvent.on(select, "change", (value) => {
-      this.localAssign(value);
+      this._currentAssign = value.target.value;
+      this.localAssign(value.target.value);
     });
 
-    this._populateAgentSelect(select, target.assignedTo);
+    this._populateAgentSelect(select, this._currentAssign);
 
     return html;
   },
@@ -125,16 +137,16 @@ const AssignDialog = WDialog.extend({
   localAssign: function (value) {
     const operation = getSelectedOperation();
     if (this._type == "Marker") {
-      operation.assignMarker(this._targetID, value.srcElement.value);
+      operation.assignMarker(this._targetID, value);
     }
     if (this._type == "Link") {
-      operation.assignLink(this._targetID, value.srcElement.value);
+      operation.assignLink(this._targetID, value);
     }
     if (this._type == "Anchor") {
       const links = operation.getLinkListFromPortal(this.options.target);
       for (const l of links) {
         if (l.fromPortalId == this.options.target.id) {
-          operation.assignLink(l.ID, value.srcElement.value);
+          operation.assignLink(l.ID, value);
         }
       }
     }
