f241b4055f53b5952eae8826bba35d48a87e1c0e
le-jeu <64744459+le-jeu@users.noreply.github.com>
2022-04-09 13:40:59 +0000

    Deploying to dist from @ wasabee-project/Wasabee-IITC@110365d97437bb90ca7b91254dfcbe21c32d2323 🚀

--- testing/wasabee.meta.js
+++ testing/wasabee.meta.js
@@ -2,7 +2,7 @@
 // @id            Wasabee.user.js
 // @name          IITC plugin: Wasabee Draw Tools
 // @namespace     https://wasabee.rocks/
-// @version       0.21.20220409130528-testing-0ca396e
+// @version       0.21.20220409134047-testing-110365d
 // @updateURL     https://wasabee-project.github.io/Wasabee-IITC/testing/wasabee.meta.js
 // @downloadURL   https://wasabee-project.github.io/Wasabee-IITC/testing/wasabee.user.js
 // @description   Wasabee is not for dating.
